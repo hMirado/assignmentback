adeb830428484509b28b5a23a5bf5ad60baaf53b
hMirado <miradotsangy@gmail.com>
2021-04-08 20:35:28 +0300

    Authentification avec jwt et tabs pour liste assignments (rendu & non rendu - en cours)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let app = express();
 let bodyParser = require('body-parser');
 
 let assignment = require('./routes/assignments');
+let authentication = require('./routes/authentication');
 
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -10,7 +11,8 @@ mongoose.Promise = global.Promise;
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 //const uri = 'mongodb+srv://mb:P7zM3VePm0caWA1L@cluster0.zqtee.mongodb.net/assignments?retryWrites=true&w=majority';
-const uri = 'mongodb+srv://hMirado:tsangy090197@clusterassignments.iulbf.mongodb.net/assignments?retryWrites=true&w=majority';
+//const uri = 'mongodb+srv://hMirado:tsangy090197@clusterassignments.iulbf.mongodb.net/assignments?retryWrites=true&w=majority';
+const uri = 'mongodb://localhost:27017/assignments';
 
 const options = {
   useNewUrlParser: true,
@@ -50,10 +52,14 @@ app.route(prefix + '/assignments')
   .post(assignment.postAssignment)
   .put(assignment.updateAssignment);
 
-
 app.route(prefix + '/assignments/:id')
   .get(assignment.getAssignment)
   .delete(assignment.deleteAssignment);
+
+/**
+ * Route authentication
+ */
+app.use(prefix + '/authentication', authentication);
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
